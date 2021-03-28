70216d9fe64c004e49cd5bff8ac07fe766e9ec3e
Roman Fihura <romanfigura120@gmail.com>
2021-03-29 00:45:15 +0300

    Updated with a sample

--- coin.cpp
+++ coin.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime> // содержит time()
+#include <ctime> // time()
+#include <vector>
+#include <fstream>
+std::vector<int> sample;
 int main()
 {
- int randomDigits[10000] = {};
+ int random;
  srand(time(NULL));
  int rewka=0, orel=0;
- for (int i = 0; i < 10000; i++)
+ for (int j=0;j<1000;j++)
  {
-  randomDigits[i] = rand()%2;
-  if (randomDigits[i] == 1)
-   orel++;
-  else
-   orel = 0;
-  if (randomDigits[i] == 0)
-   rewka++;
-  else
-   rewka = 0;
-  if (rewka == 11 || orel == 11)
+  for (int i = 0; i < 10000; i++)
   {
-   if (rewka == 11) {
-    std::cout << "Attempt number: " << i << " 11 " << "rewka in a row";
-    return 0;
-    
-   }
-   if (orel == 11) {
-    std::cout << "Attempt number: " << i << " 11 " << "orel in a row";
-    return 0;
+   random = rand() % 2;
+   if (random == 1)
+    orel++;
+   else
+    orel = 0;
+   if (random == 0)
+    rewka++;
+   else
+    rewka = 0;
+   if (rewka == 11 || orel == 11)
+   {
+    if (rewka == 11) {
+     sample.push_back(i);
+     break;
+    }
+    if (orel == 11) {
+     sample.push_back(i);
+     break;
+    }
    }
   }
  }
+ std::ofstream fout("sample.txt"); 
+ for (int n = 0; n < sample.size();n++) {
+  std::cout << n + 1 << ". " << "11 in a row on Attempt number: " << sample[n] << std::endl;
+  fout << n + 1 << ". " << "11 in a row on Attempt number: " << sample[n] << std::endl;
+ }
+ fout.close(); 
  return 0;
 }
